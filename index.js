4d7f86dfb0b953f7716376ab4858a51ddf74381d
Christina <38539756+crbarning@users.noreply.github.com>
2022-01-27 14:15:15 -0500

    Fixed API

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const inputField = document.querySelector("[name=name]");
 
-function renderCards(response) {
-  const cardArray = response.results;
+function renderPokemon(response) {
+  //console.log(response);
+  const results = response.sprites;
   let htmlTemplate = "";
-  for (let results of cardArray) {
-    htmlTemplate += `
-    <div class="card">
-    <h4>${results.name}</h4>
-    <img src="${results.url.sprites.official - artwork.front_default}" alt="${
-      results.name
-    }">
-    </div>
-    `;
-  }
-  document.querySelector(".js-pokedex").innerHTML = htmlTemplate;
+  htmlTemplate += `
+  <div class="card">
+    <h4>${response.name}</h4>
+    <img src="${results.other.dream_world.front_default}"
+    alt="${response.name}" />
+  </div>
+`;
+
+  const pokeContainer = document.querySelector(".pokedex");
+  pokeContainer.innerHTML = htmlTemplate;
 }
-function loadData(event) {
+
+function loadPokemon(event) {
   event.preventDefault();
   const searchExpression = inputField.value;
   inputField.value = "";
-  let API_URL = `https://pokeapi.co/api/v2/pokemon/`;
-
+  let API_URL = `https://pokeapi.co/api/v2/pokemon`;
   if (searchExpression.length > 0) {
-    API_URL += `?name=${searchExpression}`;
+    //console.log(searchExpression);
+    API_URL += `/${searchExpression}`;
   }
+  //console.log(API_URL);
   fetch(API_URL)
     .then((data) => data.json())
-    .then(renderCards);
+    .then((d) => renderPokemon(d));
 }
+
+document.getElementById("js-form").addEventListener("submit", loadPokemon);
+
